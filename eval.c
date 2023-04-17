c7d8e0c8cd478bc6ef446f1280b60628312d3085
Yiyu Zhou <yzhou155@vlab00.cs.usfca.edu>
2023-04-16 22:58:08 -0700

    Refactor eval.c using switch

--- eval.c
+++ eval.c
@@ -7,24 +7,26 @@ float eval(struct parse_node_st *node)
 	} else if (node->type == EX_OPER2) {
 		float l = eval(node->oper2.left);
 		float r = eval(node->oper2.right);
-		if (node->oper2.oper == OP_PLUS)
-			return l + r;
-		else if (node->oper2.oper == OP_MINUS)
-			return l - r;
-		else if (node->oper2.oper == OP_MULT)
-			return l * r;
-		else if (node->oper2.oper == OP_DIV)
-			return l / r;
-		else if (node->oper2.oper == OP_LSHIFT)
-			return (int) l << (int) r;
-		else if (node->oper2.oper == OP_RSHIFT)
-			return (int) l >> (int) r;
-		else if (node->oper2.oper == OP_BITAND)
-			return (int) l & (int) r;
-		else if (node->oper2.oper == OP_BITOR)
-			return (int) l | (int) r;
-		else if (node->oper2.oper == OP_BITXOR)
-			return (int) l ^ (int) r;
+		switch (node->oper2.oper) {
+			case OP_PLUS:
+				return (l + r);
+			case OP_MINUS:
+				return (l - r);
+			case OP_MULT:
+				return (l * r);
+			case OP_DIV:
+				return (l / r);
+			case OP_LSHIFT:
+				return ((int) l << (int) r);
+			case OP_RSHIFT:
+				return ((int) l >> (int) r);
+			case OP_BITAND:
+				return ((int) l & (int) r);
+			case OP_BITOR:
+				return ((int) l | (int) r);
+			case OP_BITXOR:
+				return ((int) l ^ (int) r);
+		}
 	} else if (node->type == EX_OPER1) {
 		float operand_val = eval(node->oper1.operand);
 		switch (node->oper1.oper) {
